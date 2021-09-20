dd24eb5d3b7043a38064a9ec24e986dd937142da
Cahit Atilgan <cahit@atilgan.ch>
2021-09-20 21:39:24 +0200

    Added new backend models

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,10 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '69f4f97cb8af322c26dd6c40ae198a87'),
     },
+    autoReload: {enabled: true}
   },
 });
