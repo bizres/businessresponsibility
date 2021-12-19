d37575d86630e6bd6e8f0a05d8f26e02f81ea90a
Cahit Atilgan <cahit@atilgan.ch>
2021-12-19 13:01:53 +0100

    Improved the doughnut chart design, using recharts library now.

--- frontend/src/components/statistics/index.tsx
+++ frontend/src/components/statistics/index.tsx
@@ -1,66 +1,53 @@
 import {tw} from 'twind';
-import {Doughnut} from 'react-chartjs-2';
+import {PieChart, Pie, Cell, ResponsiveContainer} from 'recharts';
 
-const options = {
-  plugins: {
-    tooltip: {
-      displayColors: false,
-      backgroundColor: 'rgb(255,230,155)',
-      padding: 8,
-      bodyColor: 'rgb(93,90,90)',
-      bodySpacing: 6,
-      bodyFont: {
-        size: 16
-      }
-    },
-    legend: {
-      display: false,
-      align: 'start' as 'start',
-      labels: {
-        color: 'rgba(68,64,64,0.87)',
-        padding: 24,
-        font: {
-          size: 18
-        }
-      }
-    }
-  }
+const data = [
+  {name: '136  Unternehmen berichten über einzelne Themen', value: 136},
+  {name: '89  Unternehmen berichten über einzelne Themen', value: 89},
+  {name: '528  Unternehmen haben keinen Bericht', value: 528},
+  {name: 'Für 143 Unternehmen ist der Berichtsstatus unbekannt', value: 143},
+];
+
+const COLORS = ['#FFE69AFF', '#f1b605', '#919191', '#c2c2c2'];
+const renderLabel = ({cx, cy, midAngle, innerRadius, outerRadius, value, index}) => {
+  const RADIAN = Math.PI / 180;
+  const radius = 25 + innerRadius + (outerRadius - innerRadius);
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="#6B6A73FF"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {data[index].name}
+    </text>
+  );
 };
-
-const data = {
-  labels: [
-    'For 143 companies, the report status is unknown',
-    '89 companies have a complete report for 2020',
-    '136 companies have submitted a partial report for the year 2020',
-    '528 companies have not yet reported for 2020'
-  ],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [143, 89, 136, 528],
-      backgroundColor: [
-        'rgb(242,242,242)',     // unknown
-        'rgb(255,192,2)',       // fully reported
-        'rgb(255,230,154)',     // partially reported
-        'rgb(192,192,192)'      // not reported
-      ],
-      borderColor: [
-        'rgba(0,0,0,0)',
-        'rgba(0,0,0,0)',
-        'rgba(0,0,0,0)',
-        'rgba(0,0,0,0)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
 const Statistics = () => (
   <section className={tw(`overflow-hidden`)}>
-    <div className={tw(`pt-12 pb-8 mx-auto text-center item-center`)}>
-        <div className={tw(`w-1/4 mx-auto`)}>
-            <Doughnut data={data} options={options} />
-        </div>
+    <div className={tw(`mt-12 mb-10 mx-auto text-center item-center`)}>
+      <div className={tw(`h-96 mx-auto`)}>
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              dataKey="value"
+              isAnimationActive={true}
+              data={data}
+              innerRadius={60}
+              outerRadius={150}
+              fill="#8884d8"
+              label={renderLabel}
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   </section>
 );
